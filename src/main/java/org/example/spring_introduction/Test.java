f46a97ba60370e09aa3b5333653b41db185a3b3d
Kradeyes <yanin.r.a@mail.ru>
2021-05-14 15:05:57 +0300

    Внедрение строк и других значений

--- src/main/java/org/example/spring_introduction/Test.java
+++ src/main/java/org/example/spring_introduction/Test.java
@@ -9,6 +9,8 @@ public class Test {
 
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
         context.close();
     }
 }
