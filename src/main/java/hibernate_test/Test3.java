eaf4bfb2f7f80dd9ca63aec1dd4a0b4c0c421a8c
Kradeyes <yanin.r.a@mail.ru>
2021-06-04 15:16:50 +0300

    Изменение Java объектов в БД

--- src/main/java/hibernate_test/Test3.java
+++ src/main/java/hibernate_test/Test3.java
@@ -1,4 +1,23 @@
 package hibernate_test;
 
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Test3 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+        //Employee emp = session.get(Employee.class, 2);
+        //emp.setName("Ivan");
+        //emp.setSalary(1000);
+        session.createQuery("update Employee set salary=900").executeUpdate();
+        session.getTransaction().commit();
+        factory.close();
+    }
 }
